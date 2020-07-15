9e4ae39ea56d8748fe47561de79e0ae986044809
zhanysch <zhanysch@gmail.com>
2020-07-15 22:31:47 +0600

    without sharedp preference

--- app/src/main/java/com/example/massiv/MainActivity.kt
+++ app/src/main/java/com/example/massiv/MainActivity.kt
@@ -32,7 +32,23 @@ class MainActivity : AppCompatActivity() {
 
         initView()
         initClicks()
+        initsumm()
 
+    }
+
+
+    private fun initClicks(){
+
+        update?.setOnClickListener {
+            initsumm()
+
+
+        }
+
+    }
+
+
+    private fun initsumm(){
 
 
         Edmassiv1?.setText(massiv[0].toString())
@@ -46,61 +62,37 @@ class MainActivity : AppCompatActivity() {
         val d=4
         val p =3.14
         val c= d*p
-
         Texmassiv1?.text= c.toString()
+        massiv[0]=Edmassiv1?.text.toString().toInt()
 
 
         val d1=45
         val p1=3.14
         val c1 =d1*p1
-
+        massiv[1]=Edmassiv2?.text.toString().toInt()
         Texmassiv2?.text= c1.toString()
 
         val d2 =234
         val p2 = 3.14
         val c2 = d2*p2
         Texmassiv3?.text= c2.toString()
+        massiv[2]=Edmassiv3?.text.toString().toInt()
 
         val d3 = 234
         val p3 =3.14
         val c3 = d3*p3
         Texmassiv4?.text=c3.toString()
-
+        massiv[3]=Edmassiv4?.text.toString().toInt()
         val d4 =23
         val p4 =3.14
         val c4 =d4*p4
         Texmassiv5?.text=c4.toString()
+        massiv[4]=Edmassiv5?.text.toString().toInt()
+
 
     }
 
 
-    private fun initClicks(){
-
-        val preference = getSharedPreferences("Preference", Context.MODE_PRIVATE)
-        update?.setOnClickListener {
-            val Edtext=Edmassiv1?.text.toString()
-            preference.edit().putString("Edtext",Edtext).apply()
-            preference.edit().putString("texmassiv1",Texmassiv1?.text.toString()).apply()
-
-            val Edtext2 = Edmassiv2?.text.toString()
-            preference.edit().putString("Edtext1",Edtext2).apply()
-            preference.edit().putString("Texmassiv2",Texmassiv2?.text.toString()).apply()
-
-            val Edtext3 = Edmassiv3?.text.toString()
-            preference.edit().putString("Edtext3",Edtext3).apply()
-            preference.edit().putString("Texmassiv3",Texmassiv3?.text.toString()).apply()
-
-            val Edtext4 = Edmassiv4?.text.toString()
-            preference.edit().putString("Edtext4",Edtext4).apply()
-            preference.edit().putString("Texmassiv4",Texmassiv4?.text.toString()).apply()
-
-            val Edtext5 = Edmassiv5?.text.toString()
-            preference.edit().putString("Edtext5",Edtext5).apply()
-            preference.edit().putString("Texmassiv5",Texmassiv5?.text.toString()).apply()
-
-        }
-
-    }
 
 
     private fun initView(){
